9a237863bb2b4d6d607fb058524e3e4daaca088d
Boluwatife Abiola <abiola@sprinble.co>
2020-09-26 15:33:17 +0100

    SHowing new events modal

---
+++ src/Components/Dashboard/NewEvent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Box, Text, Flex, Center, ModalFooter, Button, Wrap, Input, FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Textarea
+} from '@chakra-ui/core';
+
+const NewEvent = ({ close }) => {
+  return (
+    <React.Fragment>
+
+      <FormControl mb="8" id="date">
+        <FormLabel color="gray.500" fontWeight="bold" fontSize="13px" mb="1">Pick a Day</FormLabel>
+        <Input height="45px" type="date" />
+        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
+      </FormControl>
+
+
+      <FormControl mb="8" id="title">
+        <FormLabel color="gray.500" fontWeight="bold" fontSize="13px" mb="1">What’s happening on this day</FormLabel>
+        <Input height="45px" type="text" placeholder="My Birthday" />
+        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
+      </FormControl>
+
+      <FormControl id="description">
+        <FormLabel color="gray.500" fontWeight="bold" fontSize="13px" mb="1">
+          Tell us more about it
+        </FormLabel>
+
+        <Textarea height="50px" />
+        {/* <FormHelperText>We'll never share your email.</FormHelperText> */}
+      </FormControl>
+
+
+
+      <Flex mt="10">
+        <Button ml="auto" fontWeight="bold" onClick={close} bg="white" borderWidth="1px" fontSize="sm" py="6" my="auto" borderRadius="md">
+          Cancel
+        </Button>
+
+        <Button ml="2" fontWeight="bold" colorScheme="red" fontSize="sm" py="6" my="auto" borderRadius="md">
+          Create Event
+        </Button>
+      </Flex>
+    </React.Fragment>
+  )
+}
+
+export default NewEvent
--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { Container, Text, Flex, Button, SimpleGrid } from '@chakra-ui/core';
+// Libraries
+import { Container, Text, Flex, Button, SimpleGrid, useDisclosure } from '@chakra-ui/core';
+
+// Components
 import EventCard from './EventCard';
+import Modal from './../UI/Modal';
+import NewEvent from './NewEvent';
 
 const Landing = () => {
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Container my={10} maxW="110ch">
 
@@ -20,7 +28,7 @@ const Landing = () => {
       {/* COntent */}
       <main>
         <SimpleGrid columns={[1, 2, 3]} spacing="20px">
-          <Button as="center" bg="white" cursor="pointer" flexDir="column" borderStyle="dashed" borderWidth="2px" borderRadius="md" minH="250px" p="6">
+          <Button onClick={onOpen} as="center" bg="white" cursor="pointer" flexDir="column" borderStyle="dashed" borderWidth="2px" borderRadius="md" minH="250px" p="6">
             <Text>
               <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10 8.75V3.75M20 8.75V3.75M8.75 13.75H21.25M6.25 26.25H23.75C24.413 26.25 25.0489 25.9866 25.5178 25.5178C25.9866 25.0489 26.25 24.413 26.25 23.75V8.75C26.25 8.08696 25.9866 7.45107 25.5178 6.98223C25.0489 6.51339 24.413 6.25 23.75 6.25H6.25C5.58696 6.25 4.95107 6.51339 4.48223 6.98223C4.01339 7.45107 3.75 8.08696 3.75 8.75V23.75C3.75 24.413 4.01339 25.0489 4.48223 25.5178C4.95107 25.9866 5.58696 26.25 6.25 26.25Z" stroke="#B16B6D" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -36,6 +44,30 @@ const Landing = () => {
       </main>
 
 
+      <Modal
+        isOpen={isOpen}
+        size="lg"
+        onClose={onClose}
+        headerChildren={
+          <Flex py="4" px="4" color="#B16B6D">
+            {/* Icon */}
+            <Text my="auto" fontSize="sm">
+              <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="48" height="48" rx="24" fill="currentColor" fill-opacity="0.41" />
+                <path d="M20 19V15M28 19V15M19 23H29M17 33H31C31.5304 33 32.0391 32.7893 32.4142 32.4142C32.7893 32.0391 33 31.5304 33 31V19C33 18.4696 32.7893 17.9609 32.4142 17.5858C32.0391 17.2107 31.5304 17 31 17H17C16.4696 17 15.9609 17.2107 15.5858 17.5858C15.2107 17.9609 15 18.4696 15 19V31C15 31.5304 15.2107 32.0391 15.5858 32.4142C15.9609 32.7893 16.4696 33 17 33Z" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+
+            </Text>
+
+            <Text fontWeight="900" fontSize="lg" my="auto" ml="2">
+              Create Event
+            </Text>
+          </Flex>
+        }
+        bodyPX="10"
+      >
+        <NewEvent close={onClose} />
+      </Modal>
     </Container>
   )
 }
